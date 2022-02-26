8a1f4ed7abce015657144204893255d07424e9f1
ancnnr <ancnnr@gmail.com>
2022-02-26 15:24:26 -0400

    canvas grid now working with varaible sizing and clearning of canvas

---
+++ main.js
@@ -0,0 +1,44 @@
+
+function setCanvas() {
+    clearCanvas();
+
+
+    let input = parseInt(document.getElementById('userInput').value);
+    let pixelMeasure = 978.0/input;
+
+    for(i=0; i<input; i++)
+    {
+        const newRow = document.createElement('div');
+        newRow.classList.add('row');
+        newRow.style.height = pixelMeasure + 'px';
+        document.querySelector('.canvas').appendChild(newRow);
+
+        
+        for(j=0; j<input; j++)
+        {
+            const newPixel = document.createElement('div');
+            newPixel.classList.add('pixel');
+            newPixel.style.width = pixelMeasure + 'px';
+            newRow.appendChild(newPixel);
+        }
+    }
+}
+
+function clearCanvas()
+{
+ 
+    const canvas = document.querySelector('.canvas');
+
+    while(canvas.firstChild) {
+
+        const row = canvas.lastChild;
+
+        while(row.firstChild) {
+            row.removeChild(row.lastChild)
+        }
+
+
+        canvas.removeChild(row);
+    }
+}
+
